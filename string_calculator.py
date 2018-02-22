450d5f68fb8c9ed563452bcc6b561cfb3fb63b15
Jose <jmgallego@alea-soluciones.com>
2018-02-22 11:01:05 +0100

    Red

--- string_calculator.py
+++ string_calculator.py
@@ -7,11 +7,21 @@ def string_calculator(string):
 from expects import expect, equal
 
 AN_EMPTY_STRING = ''
+NUMBER_WITH_NONE = '"","5"'
 
 with description('Given numbers as string'):
     with description('returns the sum of the numbers separated by comma'):
-        with it('empty string'):
+        with it('given an empty string returns 0 in string format'):
 
             result = string_calculator(AN_EMPTY_STRING)
             expected_result = '0'
             expect(result).to(equal(expected_result))
+
+        with description('given none and a number separated by commma'):
+            with it('None equals to 0 and give the sum'):
+
+                result = string_calculator(NUMBER_WITH_NONE)
+                expected_result = '5'
+                expect(result).to(equal(expected_result))
+
+
